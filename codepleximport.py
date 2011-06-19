8ee577518f21143a41777ebee22e265d5b70fcf4
Mendhak <smaltaf@msn.com>
2011-06-19 15:22:22 +0100

    Parses codeplex work items, pushes to github issues with comments

--- codepleximport.py
+++ codepleximport.py
@@ -1,26 +1,46 @@
 #! /usr/bin/python
 
+
+#Download the github2 package from http://packages.python.org/github2/
+
+#User specific values
+CODEPLEX_PROJECT = "gpslogger.codeplex.com"
+GITHUB_PROJECT = "Codeplex-Issues-Importer" 
+GITHUB_USERNAME = "mendhak"
+GITHUB_ISSUELABEL = "CodePlex"
+GITHUB_APITOKEN = ""
+
+if GITHUB_APITOKEN == "":
+	raise Exception("You haven't supplied an API Token in this file")
+
+
 import urllib2
 import HTMLParser
 import re
+from github2.client import Github
+
 
 def getTupleValue(lst, key):
+	"""From a given list of tuples, searches for the key and returns its corresponding value
+		List should be of the form [(a,b),(c,d),(e,f)]"""
 	for k, v in lst:
 		if k == key:
 			return v
 
 
 class CodePlexWorkItem():
+	"""Class to represent a single workitem on codeplex"""
 	heading = ""
 	description = ""
 	comments = []
 	submittedby = ""
+	isClosed = False
 
 	def __init__(self):
-		pass
+			self.comments = []
 	
-	def SetHeading(self, h):
-		self.heading = h
+	def AppendHeading(self, h):
+		self.heading = self.heading + h
 	
 	def SetDescription(self, d):
 		self.description = d
@@ -32,14 +52,18 @@ class CodePlexWorkItem():
 		self.submittedby =  s
 
 	def AddComment(self, c):
-		self.comments.append(c)
+		self.comments.insert(0,c)
+		
+	def SetIsClosed(self, c):
+		self.isClosed = c
 
 
 class IssuesListParser(HTMLParser.HTMLParser):
-
+	"""Uses HTMLParser to parse the /workitem/list/basic page for workitem links to follow"""
 	itemLinks = []
 
 	def __init__(self, url):
+		"""Begins the parsing on the given URL"""
 		HTMLParser.HTMLParser.__init__(self)
 		request = urllib2.urlopen(url)
 		self.feed(request.read())
@@ -49,7 +73,7 @@ class IssuesListParser(HTMLParser.HTMLParser):
 	def handle_starttag(self, tag, attrs):
 		if tag == "a" and len(attrs) > 0:
 			href = getTupleValue(attrs, "href")
-
+			
 			if re.match("(.)*/workitem/[0-9]+", href):
 				self.itemLinks.append(href)
 
@@ -58,6 +82,7 @@ class IssuesListParser(HTMLParser.HTMLParser):
 
 
 class WorkItemParser(HTMLParser.HTMLParser):
+	"""Class to handle parsing a /workitem/XXXX page for information"""
 	titleFound = False
 	descriptionFound = False
 
@@ -66,12 +91,16 @@ class WorkItemParser(HTMLParser.HTMLParser):
 	comment = ""
 	
 	submittedByFound = False
+	
+	itemStatusFound = False
 
-	wi = CodePlexWorkItem()
+	currentWorkItem = None
 
 	def __init__(self, url):
+		"""Begins the parsing process"""
 		HTMLParser.HTMLParser.__init__(self)
 		request = urllib2.urlopen(url)
+		self.currentWorkItem = CodePlexWorkItem()
 		self.feed(request.read())
 
 	def handle_starttag(self, tag, attrs):
@@ -86,6 +115,8 @@ class WorkItemParser(HTMLParser.HTMLParser):
 				self.descriptionFound = True
 			elif divId != None and "MessageLabel" in divId:
 				self.commentFound = True
+			elif divId != None and "VotedLabel" in divId:
+				self.itemStatusFound = True
 
 		if tag == "a" and len(attrs) > 0:
 			aId = getTupleValue(attrs, "id")
@@ -97,15 +128,19 @@ class WorkItemParser(HTMLParser.HTMLParser):
 
 	def handle_data(self, data):
 		if self.titleFound:
-			self.wi.SetHeading(data)
+			self.currentWorkItem.AppendHeading(data)
 		if self.descriptionFound:
-			self.wi.AppendDescription(data)
+			self.currentWorkItem.AppendDescription(data)
 		if self.commentByFound:
-			self.comment = self.comment + data
+			self.comment = self.comment + "<b>" + data + "[CodePlex]</b>"
 		if self.commentFound:
 			self.comment = self.comment + data
 		if self.submittedByFound:
-			self.wi.SetSubmittedBy(data)
+			self.currentWorkItem.SetSubmittedBy(data)
+		if self.itemStatusFound:
+			if data == "closed":
+				self.currentWorkItem.SetIsClosed(True)
+			
 
 	def handle_endtag(self, tag):
 		if self.titleFound and tag == "span":
@@ -116,36 +151,80 @@ class WorkItemParser(HTMLParser.HTMLParser):
 			self.commentByFound = False
 		if self.commentFound and tag == "div":
 			self.commentFound = False
-			self.wi.AddComment(self.comment)
+			self.currentWorkItem.AddComment(self.comment)
 			self.comment = ''
 		if self.submittedByFound:
 			self.submittedByFound = False
 
 	def handle_entityref(self, name):
 		if self.descriptionFound:
-			self.wi.AppendDescription(name)
+			self.currentWorkItem.AppendDescription(name)
 		if self.commentFound:
 			self.comment = self.comment + data
+		if self.titleFound:
+			self.currentWorkItem.AppendHeading(name)
+			
 
 	def handle_charref(self, name):
 		if self.descriptionFound:
-			self.wi.AppendDescription("&#" + name)
+			self.currentWorkItem.AppendDescription("&#" + name + ";")
 		if self.commentFound:
-			self.comment = self.comment + "&#" + name
+			self.comment = self.comment + self.unescape("&#" + name + ";")
+		if self.titleFound:
+			self.currentWorkItem.AppendHeading(self.unescape("&#" + name + ";"))
+			
+	def handle_startendtag(self, tag, attrs):
+		"""For self closing tags such as <img /> or <br />"""
+		if tag == "br":
+			if self.descriptionFound:
+				self.currentWorkItem.AppendDescription("<br />")
+			if self.commentFound:
+				self.comment = self.comment + " <br />"
+		
 
 
-theUrl = "http://gpslogger.codeplex.com/workitem/list/basic?size=2147483647"
-print "Querying", theUrl
+issuesUrlFormat = "http://" + CODEPLEX_PROJECT + "/workitem/list/basic?field=CreationDate&direction=Ascending&issuesToDisplay=All&keywords=&emailSubscribedItemsOnly=false&size=100&page={pn}"
+
+continuePaging = True
+pageNumber = -1
+totalLinks = 0
 
 #Extract links from the main issues page
-#ilp = IssuesListParser(theUrl)
+while continuePaging:
+	pageNumber += 1
+	issuePageUrl = issuesUrlFormat.format(pn=str(pageNumber)) 
+	print "Parsing page ", pageNumber, "(", issuePageUrl, ")"
 
+	ilp = IssuesListParser(issuePageUrl)
+
+	if len(ilp.itemLinks) == totalLinks:
+		continuePaging = False
+		print "Reached end of issue pages"
+	else:
+		totalLinks = len(ilp.itemLinks)
+		continuePaging = True
+
+print len(ilp.itemLinks), " work item links found"
+
+parsedWorkItems = []
 
 #Loop through, process each work item link
-#for itemUrl in ilp.itemLinks:
-#	WorkItemParser(itemUrl)
-workItem = CodePlexWorkItem()
-wip = WorkItemParser("http://gpslogger.codeplex.com/workitem/7465")
-print wip.wi.submittedby
-			
+for itemUrl in ilp.itemLinks:
+	wiParser = WorkItemParser(itemUrl)
+	print wiParser.currentWorkItem.heading
+	parsedWorkItems.append(wiParser.currentWorkItem)
+	
+print len(parsedWorkItems), " work items parsed from CodePlex"
 
+#initialize github
+gh = Github(username=GITHUB_USERNAME, api_token=GITHUB_APITOKEN,requests_per_second=0.5)
+
+for wi in parsedWorkItems:
+	newIssue = gh.issues.open(GITHUB_USERNAME + "/" + GITHUB_PROJECT, title=wi.heading, body=wi.description)
+	gh.issues.add_label(GITHUB_USERNAME + "/" + GITHUB_PROJECT, newIssue.number, GITHUB_ISSUELABEL)
+	for c in wi.comments:
+		gh.issues.comment(GITHUB_USERNAME + "/" + GITHUB_PROJECT, newIssue.number, c)
+	if wi.isClosed:
+		gh.issues.close(GITHUB_USERNAME + "/" + GITHUB_PROJECT, newIssue.number)
+	print "Created Github issue", newIssue.number, "for", "[" + wi.heading + "]"
+	
